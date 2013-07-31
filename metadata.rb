8adabd6a1c1a2e7900fae20f35faa1cd287ea0f5
Ben Plunkert <ben.plunkert@blackboard.com>
2013-07-31 12:59:12 -0700

    releasing mongodb 1.0.2 for the basics -- ready for single server deploys

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "edelight GmbH"
 maintainer_email  "markus.korn@edelight.de"
 license           "Apache 2.0"
 description       "Installs and configures mongodb"
-version           "1.0.1"
+version           "1.0.2"
 
 recipe "mongodb", "Installs and configures a single node mongodb instance"
 recipe "mongodb::10gen_repo", "Adds the 10gen repo to get the latest packages"
