d25fb0513810dea144c143f7debc93bedace1a85
Ben Plunkert <ben.plunkert@blackboard.com>
2013-07-31 11:05:42 -0700

    Revert "removing confusing auth attribute from attributes/default.rb"
    This reverts commit 36c6b7a056d9471d863ea036ba8a7c909a4a7cfc.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 
+default[:mongodb][:auth] = false
 default[:mongodb][:keyfile] = nil
 default[:mongodb][:dbpath] = "/var/lib/mongodb"
 default[:mongodb][:logpath] = "/var/log/mongodb"
